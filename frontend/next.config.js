1db661e96d43caf43dbdbc1f86cc02b7ca548c4c
richard-kim-79 <blueslap0701@gmail.com>
2025-04-23 13:23:00 +0900

    Re-add webpack alias config to explicitly resolve '@'

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,20 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // webpack: (config) => {
-  //   // 절대 경로 alias 설정
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     '@': path.resolve(__dirname),
-  //   };
-  //   // 모듈 해석 루트에 프로젝트 루트 추가
-  //   config.resolve.modules = [
-  //     path.resolve(__dirname),
-  //     'node_modules',
-  //     ...(config.resolve.modules || []),
-  //   ];
-  //
-  //   return config;
-  // },
+  webpack: (config) => {
+    // tsconfig.json의 paths 설정을 webpack alias에 직접 매핑
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname), // '@'를 frontend 디렉토리의 절대 경로로 설정
+    };
+    // 중요: node_modules도 해석 경로에 포함되어야 함
+    config.resolve.modules.push(path.resolve(__dirname, 'node_modules'));
+
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
