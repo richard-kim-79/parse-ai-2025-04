b17dcb4c0948343f7a021e6e531d3dd7a17f6a2a
richard-kim-79 <blueslap0701@gmail.com>
2025-04-23 13:18:49 +0900

    Revert to using tsconfig paths and simplify next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,21 +3,21 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    // 절대 경로 alias 설정
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-    };
-    // 모듈 해석 루트에 프로젝트 루트 추가
-    config.resolve.modules = [
-      path.resolve(__dirname),
-      'node_modules',
-      ...(config.resolve.modules || []),
-    ];
-
-    return config;
-  },
+  // webpack: (config) => {
+  //   // 절대 경로 alias 설정
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@': path.resolve(__dirname),
+  //   };
+  //   // 모듈 해석 루트에 프로젝트 루트 추가
+  //   config.resolve.modules = [
+  //     path.resolve(__dirname),
+  //     'node_modules',
+  //     ...(config.resolve.modules || []),
+  //   ];
+  //
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig; 
